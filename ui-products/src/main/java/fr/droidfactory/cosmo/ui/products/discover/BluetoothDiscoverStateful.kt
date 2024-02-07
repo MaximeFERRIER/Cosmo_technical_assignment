49612075cff054b53f947ce8bd326a54adea0b14
Maxime <max.ferrier@free.fr>
2024-02-07 17:50:54 +0100

    :construction: add arrow animation

--- ui-products/src/main/java/fr/droidfactory/cosmo/ui/products/discover/BluetoothDiscoverStateful.kt
+++ ui-products/src/main/java/fr/droidfactory/cosmo/ui/products/discover/BluetoothDiscoverStateful.kt
@@ -6,19 +6,30 @@ import android.bluetooth.BluetoothDevice
 import android.content.Intent
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDownward
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
@@ -27,17 +38,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -46,6 +58,7 @@ import androidx.lifecycle.flowWithLifecycle
 import fr.droidfactory.cosmo.sdk.core.models.CosmoExceptions
 import fr.droidfactory.cosmo.sdk.core.ui.LocalWindowSizeProvider
 import fr.droidfactory.cosmo.sdk.designsystem.components.DsBluetoothCard
+import fr.droidfactory.cosmo.sdk.designsystem.components.DsButton
 import fr.droidfactory.cosmo.sdk.designsystem.components.DsTexts
 import fr.droidfactory.cosmo.sdk.designsystem.components.DsTopBar
 import fr.droidfactory.cosmo.ui.products.R
@@ -148,6 +161,19 @@ private fun BluetoothDiscoverScreen(
             DsTopBar.NavigationTopBar(title = stringResource(id = R.string.discovery_title)) {
                 actioner(BluetoothDiscoverActions.OnBackClicked)
             }
+        }, bottomBar = {
+            val buttonText = if (isScanning) {
+                R.string.stop_scanning
+            } else {
+                R.string.start_scanning
+            }
+            DsButton.PrimaryButton(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp), text = stringResource(id = buttonText)
+            ) {
+                actioner(BluetoothDiscoverActions.OnToggleDevicesDiscovery)
+            }
         }
     ) { paddings ->
         Box(
@@ -221,19 +247,48 @@ private fun DiscoveryScreen(
     actioner: BluetoothDiscoverActioner
 ) {
     if (!isScanning && discoveredDevices.isEmpty()) {
-        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-            IconButton(
-                modifier = Modifier.background(
-                    color = MaterialTheme.colorScheme.onPrimary,
-                    shape = CircleShape
-                ),
-                onClick = { actioner(BluetoothDiscoverActions.OnToggleDevicesDiscovery) }) {
-                Icon(
-                    painter = painterResource(id = fr.droidfactory.cosmo.sdk.designsystem.R.drawable.bluetooth),
-                    contentDescription = ""
-                )
-            }
+        val animatedOffsetY = remember { mutableFloatStateOf(0f) }
+        val infiniteTransition = rememberInfiniteTransition(label = "transition")
+        val offsetAnimator = infiniteTransition.animateFloat(
+            initialValue = -15f,
+            targetValue = 15f,
+            animationSpec = infiniteRepeatable(
+                animation = tween(durationMillis = 1000, easing = LinearEasing),
+                repeatMode = RepeatMode.Reverse
+            ), label = "transition_timer"
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddings),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            DsTexts.HeadlineLarge(title = "Start here")
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+
+            Icon(
+                modifier = Modifier
+                    .size(56.dp)
+                    .offset(y = animatedOffsetY.floatValue.dp),
+                imageVector = Icons.Default.ArrowDownward,
+                contentDescription = "",
+                tint = MaterialTheme.colorScheme.onTertiary
+            )
+
         }
+
+        LaunchedEffect(Unit) {
+            snapshotFlow { offsetAnimator.value }
+                .collect { value ->
+                    animatedOffsetY.floatValue = value
+                }
+        }
+
     } else {
         Column(
             modifier = Modifier
